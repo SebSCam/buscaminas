2ff2f27d5cfc7d07d2a8c10c269a59e740dc2269
MaicolM11 <mmoarias4@gmail.com>
2022-03-23 16:02:14 -0500

    add operations

---
+++ sum-microservice/src/main/java/com/uptc/summicroservice/services/OperationService.java
@@ -0,0 +1,10 @@
+package com.uptc.summicroservice.services;
+
+public interface OperationService {
+    
+    double addition(double a, double b);
+    double subtraction(double a, double b);
+    double multiplication(double a, double b);
+    double division(double a, double b);
+
+}
