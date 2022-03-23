64cc0e3ce03ef2048e0f3afb936be0912af84ed7
SebSCam <sebas9903@outlook.com>
2022-03-23 00:26:02 -0500

    first commit

---
+++ sum-microservice/src/main/java/com/uptc/summicroservice/SumMicroserviceApplication.java
@@ -0,0 +1,25 @@
+package com.uptc.summicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class SumMicroserviceApplication {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("").allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumMicroserviceApplication.class, args);
+	}
+}
---
+++ sum-microservice/src/test/java/com/uptc/summicroservice/SumMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uptc.summicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
