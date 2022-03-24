65d3fe74f016d18ca5817252958ad68e7e3e1ba9
SebSCam <sebas9903@outlook.com>
2022-03-24 02:27:20 -0500

    frontend Angular

--- calculator-frontend/src/app/app.component.ts
+++ calculator-frontend/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'calculator-frontend';
+import { environment } from '../environments/environment';
+
+@Component({ selector: 'app-root', templateUrl: 'app.component.html' })
+export class AppComponent{
+    title = "Calculator";
+    numberA!: number;
+    numberB!: number;
+    operation!: string;
+    result: any;
+
+    constructor(private http: HttpClient) { }
+
+    solve() {    
+      
+      let body = {
+        "numberA":this.numberA,
+        "numberB":this.numberB,
+        "operation":this.operation
+      }
+        // Simple POST request with a JSON body and response type <any>
+        this.result = this.http.post(environment.endpointUrl, body)
+    }
 }
